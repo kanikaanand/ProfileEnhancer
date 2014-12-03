04a4291608a972dc9f94970c7563caa837ad8e90
Shweta <shwetadseth@gmail.com>
2014-12-02 18:37:02 -0800

    one more

--- gen/com/example/profileenhancer/R.java
+++ gen/com/example/profileenhancer/R.java
@@ -1091,12 +1091,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_settings=0x7f090042;
+        public static final int action_settings=0x7f090045;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int beginning=0x7f090016;
         public static final int button1=0x7f09003e;
         public static final int checkbox=0x7f09002d;
+        public static final int city=0x7f090042;
         public static final int collapseActionView=0x7f090010;
         public static final int company=0x7f090040;
         public static final int default_activity_button=0x7f090028;
@@ -1113,7 +1114,8 @@ containing a value of this type.
         public static final int ifRoom=0x7f090011;
         public static final int image=0x7f090027;
         public static final int jobId=0x7f09003f;
-        public static final int jobtitle=0x7f090041;
+        public static final int jobTitle=0x7f090041;
+        public static final int jobtitle=0x7f090044;
         public static final int listMode=0x7f090006;
         public static final int listViewJobs=0x7f09003c;
         public static final int list_item=0x7f090029;
@@ -1139,6 +1141,7 @@ containing a value of this type.
         public static final int showHome=0x7f09000c;
         public static final int showTitle=0x7f09000d;
         public static final int split_action_bar=0x7f09001e;
+        public static final int state=0x7f090043;
         public static final int submit_area=0x7f090039;
         public static final int tabMode=0x7f090008;
         public static final int textView1=0x7f09003d;
@@ -1199,8 +1202,10 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int list_v=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int item_view=0x7f030019;
+        public static final int list_items=0x7f03001a;
+        public static final int list_v=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1249,6 +1254,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int image=0x7f0a0010;
     }
     public static final class style {
         /** 
